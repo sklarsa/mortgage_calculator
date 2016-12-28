5eff5e312c15b07cd0dca73e17b252d5eeaee039
Steven Sklar <steve@predata.com>
2016-12-28 15:01:54 -0500

    More frontend work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,28 @@
 var gulp = require('gulp');
 
 var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css')
+var rename = require('gulp-rename')
 
 gulp.task('bootstrap', function(){
 	return gulp.src('static/lib/bootstrap/stylesheets/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest("static/lib/bootstrap/stylesheets/"))
+		.pipe(cleanCss())
+		.pipe(rename({
+			suffix: ".min",
+		}))
+		.pipe(gulp.dest("static/lib/bootstrap/stylesheets/"))
 });
+
+gulp.task('sass', function(){
+	return gulp.src('static/site/sass/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest("static/site/css/"))
+		.pipe(cleanCss())
+		.pipe(rename({
+			suffix: ".min"
+		}))
+		.pipe(gulp.dest("static/site/css/"))
+})
 
