4abf636c64a926738d54d9e7b1ad155706b7aa96
Steven Sklar <steve@predata.com>
2016-12-28 13:56:29 -0500

    Added calculation endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from yield_calcs import Mortgage, CPR, PSA, SMM
 
 
 app = Flask(__name__)
@@ -8,6 +10,39 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("base.html")
+
+
+@app.route("/api/calculate", methods=["POST"])
+def calculate():
+    data = request.get_json()
+
+    speed = None
+    if "speed" in data:
+        speed_type = data["speed"]["speed_type"]
+        speed_amt = data["speed"]["speed_amt"]
+        if speed_type == "CPR":
+            speed = CPR(speed_amt)
+        elif speed_type == "PSA":
+            speed = PSA(speed_amt)
+        elif speed_type == "SMM":
+            speed_type = SMM(speed_amt)
+        else:
+            raise Exception("Speed type {0} not supported".format(speed_type))
+
+    mtg = Mortgage(
+        data["notional"],
+        data["rate"],
+        data["months"],
+        speed=speed
+    )
+
+    return {
+        "amortization_schedule": mtg.amortization_schedule,
+        "yield": mtg.yld,
+        "wal": mtg.wal,
+        "mod_dur": mtg.mod_dur,
+        "macaulay_dur": mtg.macaulay_dur
+    }
 
 
 @app.errorhandler(500)
